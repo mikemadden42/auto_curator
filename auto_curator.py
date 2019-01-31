76087bb09ae4e5c2676ca93f73f414a4c53932b6
Michael Madden <mikemadden42@icloud.com>
2019-01-30 20:39:59 -0600

    Abstract details from main

--- auto_curator.py
+++ auto_curator.py
@@ -4,13 +4,18 @@
 import templates
 
 
-def main():
-    """Automatically curate Elastic indices"""
-    templates.process_templates()
+def curate_indices():
+    """Automatically curate indices"""
     configs = templates.get_ymls()
     for config in configs:
         print('/usr/bin/curator --dry-run --config yml/curator.yml ' + 'yml/' +
               config)
+
+
+def main():
+    """Automatically curate Elastic indices"""
+    templates.process_templates()
+    curate_indices()
 
 
 if __name__ == '__main__':
